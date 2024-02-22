b029dee8e95c43681c12cf210f610a86edf154cf
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-22 13:30:27 +0500

    Add project files.

---
+++ Mypplication.Application/Services/CarServices/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using MyApplication.Domain.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mypplication.Application.Services.CarServices
+{
+    internal interface ICarService
+    {
+        public Task<string> CreateCar(CarDTO cr);
+        public Task<string> UpdateCar(int id, CarDTO cr);
+
+
+
+    }
+}
