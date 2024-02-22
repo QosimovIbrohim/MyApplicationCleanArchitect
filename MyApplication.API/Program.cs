4d17e36de9b0fc898e8d2078dd584dc81db3c824
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-22 22:44:37 +0500

    happy nation

--- MyApplication.API/Program.cs
+++ MyApplication.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using MyApplication.Infrastruct;
 using Mypplication.Application;
 
@@ -10,30 +11,23 @@ namespace MyApplication.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             builder.Services.AddAplication();
             builder.Services.AddInfrastructure(builder.Configuration);
 
             var app = builder.Build();
-
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
